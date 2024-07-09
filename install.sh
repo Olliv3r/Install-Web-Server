da338f7b2d97862aff58ffaf4f3384a8395fdfd6
Olliv3r <oliveobom100@gmail.com>
2024-07-09 01:24:56 -0400

    Ajustes

--- install.sh
+++ install.sh
@@ -5,22 +5,23 @@
 # Author: Oliver Silva
 #
 
-packages=("php" "php-apache" "phpmyadmin" "mariadb" "apache2" "openssl" "openssl-tool")
+packages=("php" "php-apache" "phpmyadmin" "mariadb" "apache2" "openssl" "openssl-tool" "figlet")
 
 
 # BANNER
 banner() {
+	text="$1"
 	clear
-	figlet -f Remo773.flf "Install"
+	figlet -f Remo773.flf $text
 }
 
 # VERIFICA SE RODA NO AMBIENTE TERMUX
 check_os() {
 	if [ -z "$TERMUX_APP__PACKAGE_NAME" ]; then
-		echo "Only compatible with the Termux environment...[None]"
+		echo -e "\e[0mOnly compatible with the Termux environment...[\e[1;31mNone\e[0m]"
 		exit
 	fi
-	echo -e "\nEnvironment compatible with the program...[Ok]"
+	echo -e "\n\e[0mEnvironment compatible with the program...[\e[1;32mOk\e[0m]"
 
 	sleep 1
 }
@@ -28,7 +29,7 @@ check_os() {
 # VERIFICA ACESSO A MEMORIA INTERNA
 check_access_internal() {
 	while [ ! -d $HOME/storage ] ; do
-		echo -e "Allow access to internal memory...[None]\n"
+		echo -e "\e[0mAllow access to internal memory...[\e[1;31mNone\e[0m]\n"
 		termux-setup-storage
 	done
 
@@ -36,9 +37,8 @@ check_access_internal() {
 	[ ! -f /sdcard/htdocs/index.php ] && {
 		echo "<?php phpinfo(); ?>" > /sdcard/htdocs/index.php
 	}
-	[ ! -f /sdcard/htdocs/.htaccess ] && cp .htaccess /sdcard/htdocs
 
-	echo -e "\nAllow granted internal memory...[Ok]\n"
+	echo -e "\n\e[0mAllow granted internal memory...[\e[1;32mOk\e[0m]\n"
 	sleep 1
 }
 
@@ -46,17 +46,17 @@ check_access_internal() {
 check_packages() {
 	for package in ${packages[*]}; do
 		if [ ! -n "$(dpkg -l | grep $package)" ]; then
-			echo -e "Required package '$package' is not present in the system, please use the first menu option to install all necessary packages, Press the ENTER key to return to the menu...\n"; read
+			echo -e "\e[1;33mRequired package '\e[1;32m$package\e[1;33m' is not present in the system, please use the first menu option to install all necessary packages, Press the ENTER key to return to the menu...\n\e[0m"; read
 			sleep 1
 			menu
 		fi
 	done
-	echo -e "Checking packages needed for the program...[Ok]\n"
+	echo -e "\e[0mChecking packages needed for the program...[\e[1;32mOk\e[0m]\e[0m"
 }
 
 # INSTALA TODOS OS PACOTES NECESSÁRIOS
 req_install() {
-	banner
+	banner "Packages"
 	echo -e "\n\e[1;36mInstalling packages necessary to run the program...\n\e[0m"
 	
 	for package in ${packages[*]}; do
@@ -65,6 +65,8 @@ req_install() {
 		fi
 	done
 	sleep 2
+	echo -e "\e[0mPress ENTER to return to the menu...\e[0m"; read
+	menu
 }
 
 # MATA UM PROCESSO EM SEGUNDO PLANO
@@ -77,7 +79,7 @@ kill_process() {
 
 # CONFIGURA O APACHE
 configure_apache() {
-	banner
+	banner "Apache"
 	echo
 	check_packages
 
@@ -90,17 +92,17 @@ configure_apache() {
 	cp ssl/* $PREFIX/etc/apache2/ssl
 	cp extra/* $PREFIX/etc/apache2/extra
 	cp phpmyadmin/* $PREFIX/etc/phpmyadmin
-	echo -e "Apache has been configured, press ENTER to return to the menu...\n"; read
+	echo -e "\e[0mApache has been configured, press ENTER to return to the menu...\n"; read
 	menu
 }
 
 # CONFIGURA O PHPMYADMIN
 configure_phpmyadmin() {
-	banner
+	banner "phpmyadmin"
 	echo
 	check_packages
 
-	echo -e "\nRunning mariadbd in the background, please wait...\n"
+	echo -e "\n\e[1;36mRunning mariadbd in the background, please wait...\e[0m"
 	kill_process "mariadbd"
 	mariadbd-safe -u root > /dev/null &
 	sleep 6
@@ -110,7 +112,7 @@ configure_phpmyadmin() {
 
 	echo -ne "User name: " ; read username
 	echo -ne "Password: " ; read password
-	echo -ne "\n\e[1;32mProcessing the command, please wait...\e[0m\n"
+	echo -ne "\n\e[1;36mProcessing the command, please wait...\e[0m\n"
 	sleep 1
 
 	if [ -z "$username" -o -z "$password" ]; then
@@ -134,10 +136,13 @@ configure_phpmyadmin() {
 	[ -d $PREFIX/etc/phpmyadmin ] && cp phpmyadmin/* $PREFIX/etc/phpmyadmin
 	sleep 2
 	echo -e "\n\e[1;32mYour new login to the phpmyadmin panel:\e[0m\n"
-	echo -e "\e[0mUser name: \e[2;32m$username\e[0m"
-	echo -e "\e[0mPassword: \e[2;32m$password\e[0m"
-	echo -e "\nLink: \e[28;32mhttps://localhost:8443/phpmyadmin\n\e[0m"
-	echo -e "\nDefault directory: \e[28;32m/sdcard/htdocs\n\e[0m"
+	echo -e "\e[0mUser name: \e[1;32m$username\e[0m"
+	echo -e "\e[0mPassword: \e[1;32m$password\e[0m"
+	echo -e "\e[0mLink: \e[1;32mhttps://localhost:8443/phpmyadmin\n\e[0m"
+	echo -e "\e[0mDirectory project: \e[1;32m/data/data/com.termux/files/usr/share/apache2/default-site/htdocs\n\e[0m"
+	echo -e "\e[0mStart the mariadb server with: \e[1;32mmariadbd-safe -u $username &\n\e[0m"
+	echo -e "\e[0mStop mariadb server with: \e[1;32mpkill -f /data/data/com.termux/files/usr/bin/mariadbd\n\e[0m"
+
 	kill_process "mariadbd"
 	echo -e "phpmyadmin has been configured, press ENTER to return to the menu...\n"; read
 	menu
@@ -152,7 +157,7 @@ goodbye() {
 menu() {
 	trap "goodbye" SIGTSTP SIGINT
 	clear
-	banner
+	banner "Menu IWS"
 
 	echo -e "\n\e[1;36mInstall and configure web server according to your wishes.\e[0m\n"
 
@@ -179,14 +184,13 @@ menu() {
 
 
 main() {
-	banner
+	[ ! -n "$(dpkg -l | grep figlet)" ] && { apt update && apt upgrade -yq && apt install figlet -yq; }
+	banner "Checking"
 	echo -e "\nChecking requirements to run the program correctly..."
 	sleep 1
 	check_os
 	check_access_internal
 	check_packages
-	apt update && apt upgrade -y
-	apt install figlet -yq
 	menu
 }
 main
